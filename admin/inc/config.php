be68f492638f1b286d695b7e3011f2e828098e9a
Calvin Matonsi <cmatonsi7@gmail.com>
2026-06-07 21:49:18 +0200

    Fix SSL connection for Azure

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -6,16 +6,22 @@ date_default_timezone_set('Africa/Johannesburg');
 $dbhost = 'shopcals-db.mysql.database.azure.com';
 $dbname = 'fashiony';
 $dbuser = 'calvinmatonsi';
-$dbpass = 'EDUV4891883@';
+$dbpass = 'YOUR_AZURE_DB_PASSWORD';
 
-define("BASE_URL", "https://shopcals.azurewebsites.net/");
+define("BASE_URL", "https://shopcals-c3gjcmeuf3hdexak.southafricanorth-01.azurewebsites.net/");
 define("ADMIN_URL", BASE_URL . "admin/");
 
 try {
-    $pdo = new PDO("mysql:host={$dbhost};port=3306;dbname={$dbname}", $dbuser, $dbpass, [
-        PDO::MYSQL_ATTR_SSL_CA => true,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+    $pdo = new PDO(
+        "mysql:host={$dbhost};port=3306;dbname={$dbname};charset=utf8mb4",
+        $dbuser,
+        $dbpass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
+        ]
+    );
 } catch(PDOException $exception) {
     echo "Connection error: " . $exception->getMessage();
 }
+?>
